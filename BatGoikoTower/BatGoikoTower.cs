bdf1f247e5d663b061e5ab1941e8c1570fcbb8b9
zoryx007 <zornitsa.meshkova@gmail.com>
2013-08-01 15:11:41 +0300

    C# 1 Exam 2013: BatGoiko Tower

---
+++ BatGoikoTower/BatGoikoTower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class BatGoikoTower
+{
+    static void Main()
+    {
+        int H = int.Parse(Console.ReadLine());
+        int width = H * 2;
+        for (int i = 1; i <= H; i++)
+        {
+            Console.Write(new string('.', H - i));
+            Console.Write('/');
+            if (i == 2 || i == 4 || i == 7 || i == 11 || i == 16 || i == 22 || i == 29 || i == 37)
+            {
+                Console.Write(new string('-', (width - 2 * (H - i) - 2)));
+            }
+            else
+            {
+                Console.Write(new string('.', (width - 2 * (H - i) - 2)));
+            }
+            Console.Write('\\');
+            Console.Write(new string('.', H - i));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Secrets/Secrets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Secrets
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        int specialSum = 0;
+        for (int i = (number.Length - 1); i >= 0; i = i - 2)
+        {
+            char odd = number[i];
+            int oddInt = (int)Char.GetNumericValue(odd);
+            int positionOdd = number.Length - i;
+            specialSum += oddInt * (positionOdd * positionOdd);
+        }
+        for (int j = (number.Length - 2); j >= 0; j = j - 2)
+		{
+			char even = number[j];
+            int evenInt = (int)Char.GetNumericValue(even);
+            int positionEven = number.Length - j;
+            specialSum += (evenInt * evenInt) * positionEven;
+		}
+        Console.WriteLine(specialSum);
+
+        int alphaLength = specialSum % 10;
+        if (alphaLength == 0)
+        {
+            Console.WriteLine(number + " has no secret alpha-sequence");
+        }
+        else
+        {
+            int R = specialSum % 26;
+
+            for (int k = R + 1; k <= alphaLength + R; k++)
+            {
+                int unicode = k + 64;
+
+                if (unicode > 90)
+                {
+                    unicode = unicode - 26;
+                }
+                char symbol = (char)unicode;
+                string text = symbol.ToString();
+                Console.Write(symbol);
+            }
+        }
+        Console.WriteLine();
+    }
+}
